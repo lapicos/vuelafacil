40b2500c033caeb8cf145f6a98442f5a2e623388
lapicos <lapicos@unal.edu.co>
2021-09-21 20:57:14 -0500

    Actualización 21_09_2021

--- vuelafacil/vuelafacil/urls.py
+++
@@ -1,8 +0,0 @@
-from voucher.models import Voucher
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('voucher/api/',include('voucher.urls'))
-]
